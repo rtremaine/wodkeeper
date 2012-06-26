dd58743749fc5f60ecfd05aedc43682cda897c27
Ryan Tremaine <ryan.trem@gmail.com>
2012-06-26 15:02:34 -0400

    scrolls new wodkeeper -s twitter_bootstrap mysql active_admin simple_form devise

---
+++ README.md
@@ -0,0 +1,21 @@
+# ReadMe
+
+
+## Deployment
+
+```
+ey deploy
+```
+Remove
+## Thanks
+
+The original scaffold for this application was created by [App Scrolls](http://appscrolls.org).
+
+The project was created with the following scrolls:
+
+* active_admin
+* mysql
+* rails_basics
+* twitter_bootstrap
+* simple_form
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wodkeeper::Application.config.secret_token = '0a0675eb29798e60ee9e0f4fb64b7da3665dd13bcbe56d74a16d09e2f49e022fccce1dfc98412fbb6bc5edfa67d3ccc82e57eb7956e1d4c28e32db7ffd0f7485'
